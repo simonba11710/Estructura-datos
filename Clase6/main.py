73da09df734082f34850c1850c3746a2e465d709
simonba11710 <142677084+simonba11710@users.noreply.github.com>
2024-04-13 15:35:30 +0000

    -a

---
+++ Clase6/main.py
@@ -0,0 +1,11 @@
+#Arboles binarios
+#Pilas,colas,Lista doblemente enlazadas --> Lineales
+#No lineales --> arboles binarios
+#raiz --> no tendria ningun precendente
+#nodo --> 
+#rama -->conector
+#hojas --> son nodos finales que no van a tener hijos
+#nivel --> cantidad de niveles que va a tener el arbol
+#Arboles estrictos --> va a tener 2 hijosw
+#Arvoles completos --> cada nodo va a tener 2 hijos
+#tarea crear los 3 recorridos de los arboles binarios
