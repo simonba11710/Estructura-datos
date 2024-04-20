bd1131ebd2d687b1a86071f12d9db7244a7aeb1d
simonba11710 <142677084+simonba11710@users.noreply.github.com>
2024-04-20 14:51:24 +0000

    -a

---
+++ Clase7/main.py
@@ -0,0 +1,7 @@
+#Repaso cola y pila lista enlazada con ejemeplos
+#Lista enlazada: Una lista enlazada simple es una estructura de datos dinámica que almacena elementos de forma continua
+#Ejemplo: una fila de amigos donde cada uno tiene un dato y apunta al amigo que esta al lado
+#Pila: Es una estructura de datos lineal que almacena elementos de una  continua especificica abajo hacia arriba
+#Ejemplo: son como una torre de vasos donde solo puedes poner y sacar vasos por la parte arriba
+#Cola:Estructura de datos que sigue el principio de primeor en entrar, primero en salir
+#Ejemplo:una fila en el supermercado donde el primero que llega es el primero que sale
